d7859d85389828063329d0577c983c0b7cdcc888
Aimekram <janina.byszewska@gmail.com>
2020-10-01 19:45:27 +0200

    Create React app

---
+++ frontend/src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      Hello darkness, my old friend
+    </div>
+  );
+}
+
+export default App;
